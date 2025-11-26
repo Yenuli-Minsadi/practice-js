6dfe914646c8afc4dd3cac9d408270f7b86d8caf
Yenuli Minsadi <samararatney@gmail.com>
2025-11-26 16:51:25 +0530

    update with forEach vs map vs filter methods

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ nums.forEach((num, index) => {
     console.log(num, index);
 });
 
+//map- array eke hama element ekkma map wela aluth array ekk hadenw, foreach eka ehema aluth array ekk return krnne na
+// but map eke aluth array ekk return krnw, foreach eke ehema krnnann empty array ekk hdl manually values push krnna wenna eh empty array ekt
+const newNums = nums.map((num) => {
+    return num*2;
+});
+
+console.log(newNums);
+
+//filter - condition ekk true/ false da blala eh condition eka meet wenwnn aluth array ekk hdl eka return krnw
+const newNums = nums.filter((num) => {
+    return num % 2 === 0;
+});
+
+console.log(newNums);
+
